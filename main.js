5e75ff6fcdec407f0af649665e74a8a630c927e5
eduwang <eduwang1010.dev@gmail.com>
2023-09-18 15:23:52 +0900

    error fix

--- main.js
+++ main.js
@@ -2,28 +2,27 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded',() => {
     const start = async () => {
+        // initialize MindAR 
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
             imageTargetSrc: './applications/assets/targets/course-banner.mind'
         });
-    
         const {renderer, scene, camera} = mindarThree;
-    
+
+        // create AR object
         const geometry = new THREE.PlaneGemoetry(1,1);
         const material = new THREE.MeshBasicMaterial({color: 0x0000ff, tranparent: true, opacity: 0.5});
-        
         const plane = new THREE.Mesh(geometry,material);
-        
+
+        // create anchor
         const anchor = mindarThree.addAnchor(0);
         anchor.group.add(plane); //THREE.Group
-    
-        
+
+        // start AR
         await mindarThree.start();
-    
         renderer.setAnimationLoop(()=>{
             renderer.render(scene, camera);
         });
     }
-
     start();
 });
