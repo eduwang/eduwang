c95f5f847df7622d13644a0d504208ece9ef4508
eduwang <eduwang1010.dev@gmail.com>
2023-09-25 16:46:55 +0900

    audioContent added

---
+++ ImageTracking_Course/9_AudioContents/AudioContents.js
@@ -0,0 +1,67 @@
+import {loadGLTF, loadAudio} from '../../applications/libs/loader.js';
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded',() => {
+    const start = async () => {
+
+        // initialize MindAR 
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: '../Rep_Image.mind',
+          });
+        const {renderer, scene, camera} = mindarThree;
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
+        
+        const gltf = await loadGLTF('../../applications/assets/models/musicband-raccoon/scene.gltf');
+        gltf.scene.scale.set(0.1, 0.1, 0.1);
+        gltf.scene.position.set(0, -0.4, 0);
+
+        const Anchor = mindarThree.addAnchor(0);
+        Anchor.group.add(gltf.scene);
+
+        const audioClip = await loadAudio("../../applications/assets/sounds/musicband-background.mp3");
+
+        const listner = new THREE.AudioListener();
+        const audio = new THREE.PositionalAudio(listner);
+
+        //
+
+        camera.add(listner);
+        Anchor.group.add(audio);
+
+        audio.setRefDistance(100); //값을 바꿔가면서 테스트해봐야 함
+        audio.setBuffer(audioClip);
+        audio.setLoop(true);
+
+        //Multiple Detection을 사용할 때 필요할 수 있음
+        Anchor.onTargetFound = () => {
+            console.log("on target found");
+            audio.play();
+        }
+
+        Anchor.onTargetLost = () => {
+            console.log("on target lost");
+            audio.pause();
+        }
+
+
+        //gltf.animations
+        const mixer = new THREE.AnimationMixer(gltf.scene);
+        const action = mixer.clipAction(gltf.animations[0]); //첫 번째 애니메이션 실행
+        action.play();
+
+        const clock = new THREE.Clock();
+
+        // start AR
+        await mindarThree.start();
+        renderer.setAnimationLoop(()=>{
+            const delta = clock.getDelta();
+            gltf.scene.rotation.set(0, gltf.scene.rotation.y+delta, 0); //3D 모델에 내장된 애니메이션은 아니고, 코드에서 직접 애니메이션 효과를 주는 방식 애니메이션이라기 보다는 움직이는 효과 정도?
+            mixer.update(delta); //애니메이션 매 프레임마다 불러오기
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+});
