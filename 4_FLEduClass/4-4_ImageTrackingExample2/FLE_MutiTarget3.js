5d2846935656afbb861eda24af5ae5c898fedc98
eduwang <eduwang1010.dev@gmail.com>
2023-11-01 22:45:45 +0900

    minor change

--- 4_FLEduClass/4-4_ImageTrackingExample2/FLE_MutiTarget3.js
+++ 4_FLEduClass/4-4_ImageTrackingExample2/FLE_MutiTarget3.js
@@ -91,54 +91,26 @@ document.addEventListener('DOMContentLoaded',() => {
             const raycaster = new THREE.Raycaster();
             raycaster.setFromCamera(mouse, camera);
 
-            const intersects1 = raycaster.intersectObjects([gltf1.scene], true); //console.log(scene)으로 scene의 구조를 파악해야 한다...!!
-            const intersects2 = raycaster.intersectObjects([gltf2.scene], true); 
-            const intersects3 = raycaster.intersectObjects([gltf3.scene], true); 
-            const intersects4 = raycaster.intersectObjects([gltf4.scene], true); 
-
-  
-            console.log(intersects1.length)
-            console.log(intersects2.length)
-            console.log(intersects3.length)
-            console.log(intersects4.length)
-            
-            if (intersects1.length > 0) {
-                console.log("audio 1 play")
-                audio1.play()
-            }
-
-            if (intersects2.length > 0) {
-                console.log("audio 2 play")
-                audio2.play()
-            }
-
-            if (intersects3.length > 0) {
-                console.log("audio 3 play")
-                audio3.play()
-            }
-
-            if (intersects4.length > 0) {
-                console.log("audio 4 play")
-                audio4.play()
-            }
-
+            const intersects = raycaster.intersectObjects(scene.children, true); //console.log(scene)으로 scene의 구조를 파악해야 한다...!!
+            console.log(intersects)
+            if (intersects.length > 0) {
+                let o = intersects[0].object; 
+                while (o.parent && !o.userData.clickable) {
+                  o = o.parent;
+                }
+                //console.log(o === gltf1.scene)
+                //console.log(o === gltf2.scene)
+                if (o.userData.clickable) {
+    
+                    console.log(o === gltf1.scene)
+                    console.log(o === gltf2.scene)
+                 
+                    if (o === gltf1.scene) {
+                      audio1.play();
+                    }
+                  }
+                      }
         });
-
-        gltf1Anchor.onTargetLost = () => {
-            gltf1.scene.scale.set(0,0,0)
-        }
-        gltf1Anchor.onTargetFound = () =>{
-            gltf1.scene.scale.set(0.04,0.04,0.04)
-        }
-        gltf2Anchor.onTargetLost = () => {
-            gltf2.scene.scale.set(0,0,0)
-        }
-        gltf3Anchor.onTargetLost = () => {
-            gltf3.scene.scale.set(0,0,0)
-        }
-        gltf4Anchor.onTargetLost = () => {
-            gltf4.scene.scale.set(0,0,0)
-        }
 
 
         // start AR
