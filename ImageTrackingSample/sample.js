73a2b74ee54b2f4614ea9500c3219e13950e33ae
eduwang <eduwang1010.dev@gmail.com>
2024-01-29 14:20:42 +0900

    sample

---
+++ ImageTrackingSample/sample.js
@@ -0,0 +1,30 @@
+//const THREE = window.MINDAR.IMAGE.THREE;
+import * as THREE from 'three';
+import {MindARThree} from 'mindar-image-three';
+
+document.addEventListener('DOMContentLoaded',() => {
+    const start = async () => {
+        // initialize MindAR 
+        const mindarThree = new MindARThree({
+            container: document.body,
+            imageTargetSrc: './target.mind'
+        });
+        const {renderer, scene, camera} = mindarThree;
+
+        // create AR object
+        const geometry = new THREE.PlaneGeometry(1,1);
+        const material = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5});
+        const plane = new THREE.Mesh(geometry,material);
+
+        // create anchor
+        const anchor = mindarThree.addAnchor(0);
+        anchor.group.add(plane); //THREE.Group
+
+        // start AR
+        await mindarThree.start();
+        renderer.setAnimationLoop(()=>{
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+});
