bc8e56d697d119de4de3b214a310cc6a25619d6e
eduwang <eduwang1010.dev@gmail.com>
2024-03-16 14:39:32 +0900

    protractor_Prototype1

--- 10_Measure/2_Protractor/Protractor_Main.js
+++ 10_Measure/2_Protractor/Protractor_Main.js
@@ -1,15 +1,11 @@
 import * as THREE from 'three';
-import { ARButton } from 'three/addons/webxr/ARButton.js'
 
 // Set up Three.js scene
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-// // Set up ARButton for camera access
-// document.body.appendChild(ARButton.createButton(renderer));
 
 // Create an arc
 var radius = 5;
@@ -19,7 +15,7 @@ var segments = 32;
 var arcGeometry = new THREE.ArcCurve(0, 0, radius, startAngle, endAngle, false);
 var arcPoints = arcGeometry.getPoints(segments);
 var arcGeometryLine = new THREE.BufferGeometry().setFromPoints(arcPoints);
-var arcMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+var arcMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 100 });
 var arcLine = new THREE.Line(arcGeometryLine, arcMaterial);
 scene.add(arcLine);
 
@@ -113,7 +109,7 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     updateEndAngleDisplay();
-
 }
 
 animate();
+
