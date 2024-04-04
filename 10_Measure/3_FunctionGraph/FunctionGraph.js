922cc31c425ebeb7c34068678f6ea8bdcf820549
eduwang <eduwang1010.dev@gmail.com>
2024-04-04 16:18:10 +0900

    Update FunctionGraph.js

--- 10_Measure/3_FunctionGraph/FunctionGraph.js
+++ 10_Measure/3_FunctionGraph/FunctionGraph.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 // Step 1: Set up the scene
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 1, 1000);
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
