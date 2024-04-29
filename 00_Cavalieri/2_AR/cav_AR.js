aaadfcc86caea85dc7c2bb8ce75fd417d108f65f
eduwang <eduwang1010.dev@gmail.com>
2024-04-29 23:29:33 +0900

    AR Setting

---
+++ 00_Cavalieri/2_AR/cav_AR.js
@@ -0,0 +1,32 @@
+//const THREE = window.MINDAR.IMAGE.THREE;
+import * as THREE from 'three';
+import {MindARThree} from 'mindar-image-three';
+
+document.addEventListener('DOMContentLoaded',() => {
+    const start = async () => {
+        // initialize MindAR 
+        const mindarThree = new MindARThree({
+            container: document.body, //mobile 환경 고려하면 굳이 container가 필요 없을 수도?!
+            imageTargetSrc: './target_cav.mind'
+        });
+        const {renderer, scene, camera} = mindarThree;
+
+        // create AR object
+        const geometry = new THREE.PlaneGeometry(1,1);
+        const material = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5});
+        const plane = new THREE.Mesh(geometry,material);
+
+        
+
+        // create anchor
+        const anchor = mindarThree.addAnchor(0);
+        anchor.group.add(plane); //THREE.Group
+
+        // start AR
+        await mindarThree.start();
+        renderer.setAnimationLoop(()=>{
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+});
